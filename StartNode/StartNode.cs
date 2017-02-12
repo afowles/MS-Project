7137265fd238c3f9b81e98c7745d04d03240d05c
Adam <acf5118@rit.edu>
2017-02-12 12:33:58 -0500

    Added start node project

---
+++ StartNode/StartNode.cs
@@ -0,0 +1,16 @@
+﻿using Distributed.Node;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartNode
+{
+    public class StartNode
+    {
+        public static void Main(string[] args)
+        {
+            Node.Main(new string[] { "129.21.89.207", "12345" });
+        }
+    }
+}
